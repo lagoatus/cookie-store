68a4ff77cb3e33e9b426be74c8fd80bb2bfa171a
‘lagoatus’ <aaron.beerman@gmail.com>
2016-03-24 18:50:04 -0700

    add js files and css

---
+++ pdx.js
@@ -0,0 +1,31 @@
+
+var portlandAirport = {
+  minSales: "",
+  maxSales: ""
+  function cookSales (){
+    var randomNum = Math.floor((Math.random() * 100) + 1);}
+};
+var washingtonSquare = {
+  minSales: "",
+  maxSales: ""
+  function cookSales (){
+    var randomNum = Math.floor((Math.random() * 100) + 1);}
+};
+var sellwood = {
+  minSales: "",
+  maxSales: ""
+  function cookSales (){
+    var randomNum = Math.floor((Math.random() * 100) + 1);}
+};
+var pioneerSquare = {
+  minSales: "",
+  maxSales: ""
+  function cookSales (){
+    var randomNum = Math.floor((Math.random() * 100) + 1);}
+};
+var pearlDistrict = {
+  minSales: "",
+  maxSales: ""
+  function cookSales (){
+    var randomNum = Math.floor((Math.random() * 100) + 1);}
+};
