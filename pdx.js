2d1856e1d9f46aab6b31ff95dbb2b75b8c79a767
‘lagoatus’ <aaron.beerman@gmail.com>
2016-03-27 09:25:40 -0700

    added new files with updated content

--- pdx.js
+++ pdx.js
@@ -1,31 +1,49 @@
 
 var portlandAirport = {
-  minSales: "",
-  maxSales: ""
-  function cookSales (){
-    var randomNum = Math.floor((Math.random() * 100) + 1);}
-};
+  minSales: 6,
+  maxSales: 24,
+  averageSales: 1.2,
+  portlandAirport.cookieHourlySales = function (){
+    var randomNum = Math.floor((Math.random(maxSales - minSales) * ) + );
+
+  document.getElementById("pdxSales").innerHTML = "<ul><ls>"+randomNum+"</ls></ul>"}
+}
+
 var washingtonSquare = {
-  minSales: "",
-  maxSales: ""
-  function cookSales (){
-    var randomNum = Math.floor((Math.random() * 100) + 1);}
+  minSales: "11",
+  maxSales: "38",
+  averageSales: "1.9",
+  function cookieHourlySales (){
+    var randomNum = Math.floor((Math.random() * 38) + 1);}
 };
 var sellwood = {
-  minSales: "",
-  maxSales: ""
-  function cookSales (){
-    var randomNum = Math.floor((Math.random() * 100) + 1);}
+  minSales: "20",
+  maxSales: "48",
+  averageSales: "3.3",
+  function cookieHourlySales (){
+    var randomNum = Math.floor((Math.random() * 48) + 1);}
 };
 var pioneerSquare = {
-  minSales: "",
-  maxSales: ""
-  function cookSales (){
-    var randomNum = Math.floor((Math.random() * 100) + 1);}
+  minSales: "17",
+  maxSales: "88",
+  averageSales: "5.2",
+  function cookieHourlySales (){
+    var randomNum = Math.floor((Math.random() * 88) + 1);}
 };
 var pearlDistrict = {
-  minSales: "",
-  maxSales: ""
-  function cookSales (){
-    var randomNum = Math.floor((Math.random() * 100) + 1);}
+  minSales: "3",
+  maxSales: "24",
+  averageSales: "2.6",
+  function cookieHourlySales (){
+    var randomNum = Math.floor((Math.random() * 24) + 1);}
+};
+//
+// function addSalesToTable() {
+//   var table = document.getElementById("pdxSales");
+//   for (var index = 0; index < locationSales.length; index++) {
+//     var locationSalesRow =  "<ul>"+location[index]+"<ls>".cookieHourlySales()+"</ls>";
+//     locationSalesRow += "<ls>"+locationSales[index}+"</ls>";
+//     locationSalesRow += "<ls>"+locationSales[index]+"</ls></ul>";
+//     table.innerHTML += locationSalesRow;
+
 };
